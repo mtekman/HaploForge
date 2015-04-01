1aa521d22b7283b1e75e388a685923147736bc72
Mehmet Tekman <m.tekman@ucl.ac.uk>
2015-04-01 18:42:16 +0100

    A* working well, now need an exit condition

--- JS/test.js
+++ JS/test.js
@@ -1,3 +1,7 @@
+//TODO: Incorporate numsets as the selection function to get best path:
+//       i.e.  route.length === array.length && numsets is minimal
+
+
 
 function debugconsole(condition){
 	if (condition)
@@ -9,23 +13,38 @@ function a_star_bestfirst(array, exclude_list = [])
 {
 	var end = array.length -1;
 
+	array = [
+			 {color_group: [-1]},
+			 {color_group: [7,4]},
+			 {color_group: [6,4]},
+			 {color_group: [4,-1]},
+			 {color_group: [-1]},
+			 {color_group: [7,4]},
+			 {color_group: [-1]},
+			 {color_group: [6,4]},
+		];
+
+	exclude_list = 4;
+
 	// Define excludelist function
 	var inExcludeList;
 
 	if (exclude_list.length === 0){ 						// No list given
 		inExcludeList = function (item){ return false;};
 	}
-	else if (exclude_list.length === 1){ 					// Single item given
+	else if (exclude_list.length === undefined){ 			// Single item given
 		inExcludeList = function (item){ return item === exclude_list;};
+	}
+	else if (exclude_list.length === 1){ 			// Single array item
+		inExcludeList = function (item){ return item === exclude_list[0];}
 	}
 	else{ 													// Whole list given
 		inExcludeList = function (item){ return (exclude_list.indexOf(item) !== -1);}
 	}
 
 	var MAX_ROUTES = 4;    // maximum amount of working routes
-
-	var	exploring_routes = [[]], 		// initial zero route
-		zero_indexes = [];
+	var	exploring_routes = [{array:[],numsets:0}]; 		// initial zero route
+	var route_map = {}; // routes explored already
 
 	var num_cycles = 0;
 
@@ -33,82 +52,116 @@ function a_star_bestfirst(array, exclude_list = [])
 	{
 		//Sort current open routes, and take the top batch
 		// discarding the rest
-		exploring_routes = exploring_routes.sort(function (a,b){ return b.length - a.length;}).slice(0,MAX_ROUTES);
+		exploring_routes = exploring_routes.sort(function (a,b){ return b.array.length - a.array.length;}).slice(0,MAX_ROUTES);
 
-// 		console.log("routes=", exploring_routes);
+		// Happens after the splice and sort
+ 		if (exploring_routes[0].array.length === array.length) break;
+
+ 		console.log("routes="+exploring_routes.map(function (n){ return "["+n.array+":"+n.numsets+"] "}));
 
 		if (num_cycles ++ > 10){
-			console.error(num_cycles);
+			console.log(num_cycles);
 			break;
 		}
 
+		var num_routes_now = exploring_routes.length;
 
-		for (var e=0; e < exploring_routes.length; e++)
+		for (var e=0; e < num_routes_now; e++)
 		{
 			// Current open route
-			var current_route = exploring_routes[e],
+			var current_route = exploring_routes[e].array,
 				current_row = current_route.length;
-
-// 			if (current_row === end){
-// 				continue;
-// 			}
 
 			// Various routes at this row
 			//var num_colors = iterableIndexes[current_row];
 			var num_colors = array[current_row].color_group.length
 
-// 			console.log("    num_colors=", num_colors);
+			console.log("-Route:", current_route);
+			console.log("    current_row=" + current_row);
+			console.log("    num_colors =", num_colors);
 
-			var ordered_routes = {};
+			var ordered_routes = {},
+				zero_indexes = {};
 
 			// Look ahead by one
 			for (var c=0; c< num_colors; c++){
 				var current_color = array[current_row].color_group[c];
 
+				console.log("    - trying color "+current_color);
+
+
+				if (inExcludeList(current_color)){
+					console.log("    - EXCLUDE!");
+					continue;
+				}
+
 				//Perform look ahead
-				var stretch = current_row;
-				while ( stretch <= end)
+				var stretch = current_row+1;
+				while ( stretch < end )
 				{
 					var new_colors = array[stretch].color_group;
+					console.log("       - testing "+current_color+" against "+new_colors+" @i "+stretch);
 
-					if (new_colors.indexOf(current_color) === -1)
+					// Only break on another non-zero group color
+ 					if (new_colors.length === 1){
+						if (new_colors[0] === zero_color_grp)
+							zero_indexes [stretch] = 0;
+
+					}
+					else if (new_colors.indexOf(current_color) === -1)
 						break;
-					else
-						stretch ++;
 
-					// Mark (but still count) zeros
-					if (new_colors.length === 1 && new_colors[0] === zero_color_grp)
-						zero_indexes.push( stretch );
+					stretch ++;
 				}
-				ordered_routes[stretch-current_row] = current_color; // store color
-			}
-// 			console.log("    ordered_routes=", ordered_routes);
+				console.log("    - stretches for ",stretch - current_row, "until index", stretch);
 
-			if (ordered_routes.length === 0)
-				continue;
+				// Store color with key as the length of the stretch
+				ordered_routes[ stretch - current_row ] = current_color;
+			}
+			console.log("    ordered_routes=", ordered_routes);
+
+// 			if (ordered_routes.length === 0)
+// 				continue;
 
 			var keys_rev_ord = Object.keys(ordered_routes).sort(function(a,b){return b-a});
+			console.log("    reversed_order=", keys_rev_ord);
 
 			// Add routes to current route
-			for (var k=0; k < keys_rev_ord; k++){
+			for (var k=0; k < keys_rev_ord.length; k++){
 
-				var new_r = current_route.slice(); 	//clone a new path for each fork
+				var key = keys_rev_ord[k],
+					new_r = current_route.slice(); 			//clone a new path for each fork
 
-				var len = k;
+				var len = key;
 				while(len --> 0)
-					new_r.push(  ordered_routes[k] ); 	// push the color k times
+					new_r.push(  ordered_routes[key] ); 	// push the color k times
+
+				console.log("   - adding '"+ordered_routes[key]+"' "+key+"x  to ", new_r);
 
 				//Add the zeros
-				for (var z=0; z < zero_indexes.length; z++){
-					var z_index = zero_indexes[z];
-					new_r[ z_index ] = zero_color_grp;
+//  				for (var z_index in zero_indexes){
+// 					if (new_r.length > z_index)
+// 	 					new_r[ z_index ] = zero_color_grp;
+//  				}
+
+				var string_key = new_r.reduce( function(a,b){ return a+""+b;});
+
+				if (!(string_key in route_map)){
+					route_map[string_key] = 0;
+					exploring_routes.push({array:new_r, numsets:0}); 			// push the new path if unique
 				}
-				exploring_routes.push(new_r); 			// push the new path
 			}
-			console.log("    explored=",exploring_routes);
+			console.log("    explored="+exploring_routes.map(function (n){ return "["+n+"] "}));
+			console.log("    on "+e+"/"+num_routes_now);
 		}
+		console.log("next iter");
 	}
 	console.log("best_routes A*", exploring_routes);
+	console.log(" with best=", exploring_routes[0]);
+	console.log("  in "+num_cycles+" cycles");
+
+	console.log(array.map(function (a){return ""+a.color_group+"";}));
+	throw new Error("");
 
 	return [exploring_routes[0],[]];
 }
