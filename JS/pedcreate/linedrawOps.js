4e89a04b54bf6393c0fda47f5ba9bf46db8dbe27
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-05-18 17:45:53 +0100

    finally turned linedrawOps into an inheritable class

--- JS/pedcreate/linedrawOps.js
+++ JS/pedcreate/linedrawOps.js
@@ -1,93 +1,204 @@
 // Super class of offspringDraw and matelineDraw classes
 
-lineDrawOps = {
 
-	_layer: null, /*Layer*/
-	_tmpRect: null, /* Rectangle to detect mousemove */
-	_tmpLine : null,
+class LineDrawOps {
 
-	_startPoint : {x:-1,y:-1},
+	constructor() {
+		this._layer = null; /*Layer*/
+		this._tmpRect = null; /* Rectangle to detect mousemove */
+		this._tmpLine = null;
+		this._startPoint = {x:-1,y:-1};
+
+		// Callbacks
+		// Set before _init
+		this._oncirclemousedown = null;
+		this._onaddhit = null;
+		this._ondelhit = null;		
+		this._onbeginlinedraw = null;
+		this._onbeginlinedraw_mousemove = null;
+		this._onbeginlinedraw_mouseup = null;
+		this._onendlinedraw = null;
+	}
 
 
-	_delHitRect: function(){
-		lineDrawOps._layer.destroy();
-	},
+	_delHitRect() {
 
-	_addHitRect: function(callback = null)
-	{
+		if (this._ondelhit !== null){
+			this._ondelhit();
+		}
 
-		lineDrawOps._layer = (new Kinetic.Layer({
+		this._layer.destroy();
+		this._layer = null;
+	}
+
+	_addHitRect() {
+
+		this._layer = (new Kinetic.Layer({
 			width: stage.getWidth(),
 			height:stage.getHeight(),
 			x:0, y:0
 		}));
 
-		lineDrawOps._tmpRect = (new Kinetic.Rect({
+		this._tmpRect = (new Kinetic.Rect({
 			width: stage.getWidth(),
 			height:stage.getHeight(),
 			x:0, y:0,
 		}))
 
-		stage.add( lineDrawOps._layer );
-		lineDrawOps._layer.add(lineDrawOps._tmpRect);
+		stage.add( this._layer );
+		this._layer.add(this._tmpRect);
 
-		if (callback !== null){callback();}
+		this.drawCirclesAroundActiveFam();
 
-		lineDrawOps._layer.draw();
-	},
+		if (this._onaddhit !== null){
+			this._onaddhit();
+		}
 
-	changeToArrowCursor: function(){
+		this._layer.draw();
+	}
+
+
+	static changeToArrowCursor(){
 		document.body.style.cursor = "url('assets/Precision.cur'),auto";
-	},
+	}
 
-	restoreCursor: function(){
+	static restoreCursor() {
 		document.body.style.cursor = "";
-	},
+	}
 
 
-	beginLineDraw: function(mousemovecallback, mouseupcallback = null)
-	{
-		lineDrawOps._tmpLine = new Kinetic.Line({
+	beginLineDraw() {
+		var _this = this;
+
+		_this._tmpLine = new Kinetic.Line({
 			stroke: 'black',
 			strokeWidth: 2,
 		});
 
-		lineDrawOps._layer.add(this._tmpLine);	
+		_this._layer.add(_this._tmpLine);	
 
-		lineDrawOps._tmpRect.on("mousemove", function(event){
-			mousemovecallback(event);
+		_this._tmpRect.on("mousemove", function(event){
+			if (_this._startPoint.x !== -1){
+
+				var mouseX = Math.floor(event.evt.clientX/grid_rezX)*grid_rezX,
+					mouseY = Math.floor(event.evt.clientY/grid_rezY)*grid_rezY;
+
+				changeRLineHoriz(
+					_this._tmpLine,
+					_this._startPoint,
+					{x:mouseX,y:mouseY}
+				);
+
+				if (_this._onbeginlinedraw_mousemove !== null){
+					_this._onbeginlinedraw_mousemove(); // pass this?
+				}
+	
+				_this._layer.draw();
+			}
 		});
 
+		_this._tmpRect.on("mouseup", function(event){
 
-		lineDrawOps._tmpRect.on("mouseup", function(event){
-			if (mouseupcallback === null){
-				lineDrawOps.endLineDraw();
+			if (_this._onbeginlinedraw_mouseup !== null){
+				_this._onbeginlinedraw_mouseup(); // pass this?
 			}
-			else{
-				mouseupcallback(event);
-			}
+
+			_this.endLineDraw();
 		});
+	}
 
-	},
+	endLineDraw() {
 
-	endLineDraw:function(callback =  null){
-		lineDrawOps._delHitRect();	
+		this._delHitRect();	
 		
-		if (lineDrawOps._tmpLine !== null){
-			lineDrawOps._tmpLine.destroy();
+		if (this._tmpLine !== null){
+			this._tmpLine.destroy();
+			this._tmpLine = null;
 		}
 		
-		lineDrawOps.restoreCursor();
+		LineDrawOps.restoreCursor();
 
-		if (callback !== null){
-			callback();
+		if (this._onendlinedraw !== null){
+			this._onendlinedraw(); // pass this?
 		}
-	},
 
-	init: function()
-	{
+		this._startPoint = {x:-1, y:-1}
 
-		if (lineDrawOps._tmpLine !== null){
+	}
+
+	drawCirclesAroundActiveFam(){
+
+		var _this = this;
+
+		for (var perc_id in personDraw.used_ids)
+		{
+			var personIn = personDraw.used_ids[perc_id]
+
+			if (personIn.family !== familyDraw.active_fam_group.id){
+				continue;
+			}
+
+			var gfx = personIn.gfx;
+
+			var apos = gfx.getAbsolutePosition(),
+				rad = 15 ;
+
+			var circle = new Kinetic.Circle({
+				x: apos.x, 
+				y: apos.y,
+				radius: rad*2,
+				stroke:"red",
+				strokeWidth:2.5
+			});
+			circle.id = perc_id;
+
+			circle.on("mouseover", function(event){
+				
+				if (_this._startPoint.x === -1){
+					LineDrawOps.changeToArrowCursor();
+				}
+				else { //Start point set
+
+					changeRLineHoriz(
+						_this._tmpLine,
+						_this._startPoint,
+						this.getAbsolutePosition()
+					);
+
+					_this._layer.draw();
+				}
+			});
+
+			circle.on("mouseout", function(){
+				if (_this._startPoint.x === -1){
+					LineDrawOps.restoreCursor();
+				}
+			});
+
+
+			circle.on("mousedown", function(event, callback)
+			{
+				if (_this._startPoint.x === -1){
+					var cX = this.getX(),
+						cY = this.getY();
+
+					_this._startPoint = {x:cX, y:cY};
+
+					if (_this._oncirclemousedown !== null){
+						_this._oncirclemousedown(this); // Pass circle object on
+						//  _this.childNodeID = this.id
+					}
+					_this.beginLineDraw();
+				}
+			});
+			
+			_this._layer.add(circle);
+		}
+	}
+
+
+	init(){
+		if (this._tmpLine !== null){
 			this.endLineDraw();
 		}
 
--- JS/pedcreate/offspringDraw.js
+++ JS/pedcreate/offspringDraw.js
@@ -1,114 +1,29 @@
 
 
-offspringDraw = {
+/*
+var offspringDraw = {
 
-	/*_startPoint: inherited */
-	_endPoint : {x:-1,y:-1},
+	// "Inherited"
+	_layer: lineDrawOps._layer,
+	_tmpRect: lineDrawOps._tmpRect,
+	_tmpLine : lineDrawOps._tmpLine,
+	_startPoint : lineDrawOps._startPoint,
+	_delHitRect: lineDrawOps._delHitRect,
+	_addHitRect: lineDrawOps._addHitRect,
 
-	matelineNodeID:null,
-	childNodeID:null,
-
-	_matchFound: false,
-
+	beginLineDraw: lineDrawOps.beginLineDraw,
 	init: lineDrawOps.init,
 
-	_addHitRect: function(){
-		lineDrawOps._addHitRect(function()
-		{		
-			for (var perc_id in personDraw.used_ids)
-			{
-				var personIn = personDraw.used_ids[perc_id]
+	_endPoint : {x:-1,y:-1},
+	
+	matelineNodeID: null,
+	childNodeID: null,
 
-				if (personIn.family !== familyDraw.active_fam_group.id){
-					continue;
-				}
-
-				var gfx = personIn.gfx;
-
-				var apos = gfx.getAbsolutePosition(),
-					rad = 15 ;
-
-				var circle = new Kinetic.Circle({
-					x: apos.x, 
-					y: apos.y,
-					radius: rad*2,
-					stroke:"red",
-					strokeWidth:2.5
-				});
-				circle.id = perc_id;
-
-				circle.on("mouseover", function(event){
-					
-					if (lineDrawOps._startPoint.x === -1){
-						lineDrawOps.changeToArrowCursor();
-					}
-					else { //Start point set
-						offspringDraw._matchFound = true;
-
-						changeRLineHoriz(
-							lineDrawOps._tmpLine,
-							lineDrawOps._startPoint,
-							this.getAbsolutePosition()
-						);
-
-						lineDrawOps._layer.draw();
-					}
-				});
-
-				circle.on("mouseout", function(){
-					if (lineDrawOps._startPoint.x === -1){
-						lineDrawOps.restoreCursor();
-					}
-					else{
-						offspringDraw._matchFound = false;
-					}
-				});
-
-
-				circle.on("mousedown", function(event)
-				{
-					if (lineDrawOps._startPoint.x === -1){
-						var cX = this.getX(),
-							cY = this.getY();
-
-						lineDrawOps._startPoint = {x:cX, y:cY};
-						offspringDraw.childNodeID = this.id
-
-						offspringDraw.beginLineDraw();
-					}
-					else { //Set end point
-					}
-				});
-				lineDrawOps._layer.add(circle);
-			}
-		})
-	},
-
-
-	beginLineDraw: function(){
-
-		lineDrawOps.beginLineDraw(function mousemover(event)
-		{
-			if (offspringDraw._matchFound === false){
-
-				var mouseX = Math.floor(event.evt.clientX/grid_rezX)*grid_rezX,
-					mouseY = Math.floor(event.evt.clientY/grid_rezY)*grid_rezY;
-
-				changeRLineHoriz(
-					lineDrawOps._tmpLine,
-					lineDrawOps._startPoint,
-					{x:mouseX,y:mouseY}
-				);
-		
-				lineDrawOps._layer.draw();
-			}
-		});
-	},
 
 	endLineDraw: function( matelineID ){
 		lineDrawOps.endLineDraw(function(){
 			offspringDraw.matelineNodeID = matelineID;
-			console.log("ASD")
+			console.log("stored mateline_ID")
 		});
 	},
 		
@@ -116,6 +31,8 @@ offspringDraw = {
 
 		var fam_group = unique_graph_objs[familyID].group,
 			edge_map  = unique_graph_objs[familyID].edges;
+
+		var _this = this;
 
 		for (var key in edge_map){
 			if (key[0]==='m'){
@@ -128,18 +45,18 @@ offspringDraw = {
 
 				// Lock offspring line to node if nearby
 				node.on("mouseover", function(){
-					if (offspringDraw._tmpLine !== null)
+					if (lineDrawOps._tmpLine !== null)
 					{
-						offspringDraw._endPoint = this.getPosition();
+						_this._endPoint = this.getPosition();
 					}
 				});
 
 				// Mouse up -- it's been selected
 				node.on("mouseup", function(){
-					if (offspringDraw._tmpLine !== null)
+					if (lineDrawOps._tmpLine !== null)
 					{
-						offspringDraw._endPoint = this.getPosition();
-						offspringDraw.endLineDraw( this.matelineID );
+						_this._endPoint = this.getPosition();
+						_this.endLineDraw( this.matelineID );
 					}
 				});
 
@@ -149,4 +66,4 @@ offspringDraw = {
 			}
 		}
 	}
-}
+};*/
