f0737df165f3c4ff43d7306d122acf13a97b715e
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-05-20 14:04:32 +0100

    duplicate

--- JS/familymapOps.js
+++ JS/familymapOps.js
@@ -4,14 +4,14 @@ var familyMapOps = {
 		
 		if (!(family_id in family_map)){
 			family_map[family_id] = {}
-			console.log("FMO: adding new family", family_id)
+			console.log("adding new family", family_id)
 		}
 
 		if (!(person.id in family_map[family_id])){
 			family_map[family_id][person.id] = person;
 			return 0;
 		}
-		console.log("FMO:", person_id,"already in", family_id)
+		console.log(person_id,"already in", family_id)
 		return -1;
 	},
 
@@ -22,10 +22,23 @@ var familyMapOps = {
 				delete family_map[family_id][person_id];
 				return 0;
 			}
-			console.log("FMO:", person_id,"not in", family_id)
+			console.log(person_id,"not in", family_id)
 			return -1;
 		}
-		console.log("FMO:", family_id, "not in map");
+		console.log(family_id, "not in map");
+		return -1;
+	}
+
+	getPerc: function(person_id, family_id)
+	{
+		if (family_id in family_map){
+			if (person_id in family_map[family_id]){
+				return family_map[family_id][person_id];
+			}
+			console.log(person_id,"not in", family_id)
+			return -1;
+		}
+		console.log(family_id, "not in map");
 		return -1;
 	}
 }
