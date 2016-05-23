358028e11945ba793843a18f4d86b99a3ef4bf61
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-05-23 18:10:41 +0100

    working person box

--- JS/message_box.js
+++ JS/message_box.js
@@ -104,11 +104,55 @@ var famProps = {
 
 var persProps = {
 	_box : document.getElementById('person_props'),	
-	_id : document.getElementById('pers_id'),
-	_name : document.getElementById('pers_name'),
-	_gender : document.getElementById('pers_gender'),
-	_affect : document.getElementById('pers_affect'),
+	_genderInput : document.getElementById('pers_gender'),
+	_genderUnknown: document.getElementById('pers_gender_unknown'),
+	_affectInput : document.getElementById('pers_affect'),
+	_affectUnknown : document.getElementById('pers_affect_unknown'),
 	_submit : document.getElementById('pers_submit'),
+
+	id : document.getElementById('pers_id'),
+	name : document.getElementById('pers_name'),
+	gender : -1,
+	affect: -1,
+	
+	_getGender: function(){
+		if (!(this._genderUnknown.checked)){
+			return 0;
+		}
+		else { //Box checked, Male or Female
+			return (this._genderInput.checked)?2:1;
+		}
+	},
+
+	_setGender: function(gender){
+		if (gender === 0 ){
+			this._genderUnknown.checked = false;
+		}
+		else {
+			this._genderUnknown.checked = true;
+			this._genderInput.checked = (gender===2);
+		}
+	},
+
+	_getAffect: function(){
+		if (!(this._affectUnknown.checked)){
+			return 0;
+		}
+		else { //Box checked, Unaffected or Affected
+			return (this._affectInput.checked)?2:1;
+		}
+	},
+
+	_setAffect: function(affect){
+		if (affect === 0 ){
+			this._affectUnknown.checked = false;
+		}
+		else {
+			this._affectUnknown.checked = true;
+			this._affectInput.checked = (affect===2);
+		}
+	},
+
 
 	hide: function(){ this._box.style.display = "none";},
 	show: function(){ this._box.style.display = "";},
@@ -117,19 +161,19 @@ var persProps = {
 		utility.showBG();
 		this.show();
 
-		this._id.value     = person.id;
-		//this._name.value   = person.name;
-		this._gender.value = person.gender;
-		this._affect.value = person.affected;
+		this.id.value     = person.id;
+		//this.name.value   = person.name;
+//		this._setGender(person.gender);
+//		this._setAffect(person.affected);
 	},
 
 	getProps: function(){
 		var person = {id:-1,gender:-1,affected:-1, name:""};
 
-		person.id = Number(this._id.value);
-		person.gender = Number(this._gender.value);
-		person.affected = Number(this._affect.value);
-		person.name = this._name.value;
+		person.id = Number(this.id.value);
+		person.gender = this._getGender();
+		person.affected = this._getAffect();
+		person.name = this.name.value;
 
 		this.hide();
 		utility.hideBG();
