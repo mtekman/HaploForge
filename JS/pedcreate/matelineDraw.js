2ad1c49cd4391f8e82fa97e02ffff90e9a87c9fc
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-05-24 13:31:08 +0100

    method to join user-specific Ids

--- JS/pedcreate/matelineDraw.js
+++ JS/pedcreate/matelineDraw.js
@@ -2,12 +2,12 @@
 
 class MatelineDraw extends LineDrawOps {
 
-	constructor(familyID)
+	constructor(familyID, startID = null, endID = null)
 	{
 		super(familyID);
 
-		this.startNodeID = null;
-		this.endNodeID = null;
+		this.startNodeID = startID;
+		this.endNodeID = endID;
 
 		// First click
 		this._oncirclemousedown = function(circle, group){
@@ -17,61 +17,79 @@ class MatelineDraw extends LineDrawOps {
 
 		// Second click
 		this._oncirclemousedown_final = function(circle){
-			//Add line to unique_graph_obs so that dragevents would update it
-			//But ONLY after the relationship has been set
-
-			// Rules:
-			//   1. Two types of lines
-			//      a. Mateline
-			//      b. Parentline
-			//   
-			//   2. Parentline hangs from center of Mateline
-			//      there requires a Mateline to be present before creation
-			//
-			//   3. There needs to be a temp node hanging in the center of mateline to
-			//      join offspring to.
-
-			// Mates aren't connected in family map, only in unique_graph_lines
-
 			this.endNodeID = circle.id;
 			console.log("endNodeID=", this.endNodeID)
 
-			var person1 = personDraw.used_ids[Number(this.startNodeID)],
-				person2 = personDraw.used_ids[Number(this.endNodeID)];
+			this.joinIDs()
+		}
 
-			if (person1.id === 0 || person2.id === 0){
-				console.log("Not possible");
-				return;
-			}
-
-			person1	= family_map[this._family][person1.id];
-			person2	= family_map[this._family][person2.id];
-
-			if (person1.gender === person2.gender){
-				var type = {0: 'unknowns',
-							1: 'males, not yet applicable',
-							2: 'females, not yet applicable'};
-
-				utility.notify("Error:", "Cannot join two " + type[person1.gender]);
-				return;
-			}
+		// If IDs set in constructor, just perform a join.
+		if (this.endNodeID !== null){
+			this.joinIDs();
+		}
 
 
-			var moth = (person1.gender===2)?person1:person2,
-				fath = (person1.gender===1)?person1:person2;
+	}
 
-			moth.mates.push(fath);
-			fath.mates.push(moth);
 
-			// Need to manually insert the line
-			var u_matesline = UUID('m', fath.id, moth.id);
+	joinIDs(){
+				//Add line to unique_graph_obs so that dragevents would update it
+		//But ONLY after the relationship has been set
 
-			var line_pos = this._tmpLine.getPosition(),
-				fam_group = unique_graph_objs[this._family].group,
-				group_pos = fam_group.getAbsolutePosition(),
-				new_line = this._tmpLine.clone();
+		// Rules:
+		//   1. Two types of lines
+		//      a. Mateline
+		//      b. Parentline
+		//   
+		//   2. Parentline hangs from center of Mateline
+		//      there requires a Mateline to be present before creation
+		//
+		//   3. There needs to be a temp node hanging in the center of mateline to
+		//      join offspring to.
 
-			var points = new_line.getPoints();
+		// Mates aren't connected in family map, only in unique_graph_lines
+		var person1 = personDraw.used_ids[Number(this.startNodeID)],
+			person2 = personDraw.used_ids[Number(this.endNodeID)];
+
+		if (person1.id === 0 || person2.id === 0){
+			console.log("Not possible");
+			return;
+		}
+
+		person1	= family_map[this._family][person1.id];
+		person2	= family_map[this._family][person2.id];
+
+		if (person1.gender === person2.gender){
+			var type = {0: 'unknowns',
+						1: 'males, not yet applicable',
+						2: 'females, not yet applicable'};
+
+			utility.notify("Error:", "Cannot join two " + type[person1.gender]);
+			return;
+		}
+
+
+		var moth = (person1.gender===2)?person1:person2,
+			fath = (person1.gender===1)?person1:person2;
+
+		moth.mates.push(fath);
+		fath.mates.push(moth);
+
+		// Need to manually insert the line
+		var u_matesline = UUID('m', fath.id, moth.id);
+
+		// If line does not exist -- (because ids are declared and user-set) -- create one
+		if (this._tmpLine === null){
+			console.log("creating new dummy line");
+			this._tmpLine = new Kinetic.Line({
+				stroke: 'black',
+				strokeWidth: 2,
+				points: [0,0,1,1,2,2]
+			});
+		}
+		else {
+			// Perform horizontal flip if neccesary on existing line
+			var points = this._tmpLine.getPoints();
 			//console.log(points)
 
 			if (points[0] > points[6]){
@@ -84,28 +102,35 @@ class MatelineDraw extends LineDrawOps {
 					-points[0], -points[1]
 				];
 
-				new_line.setPoints(inverted)
+				this._tmpLine.setPoints(inverted);
 			}
-			//console.log(new_line.getPoints())
-
-			fam_group.add(new_line);
-
-			var fath_gfx = unique_graph_objs[this._family].nodes[moth.id].graphics.getPosition();
-
-			new_line.setX( fath_gfx.x );
-			new_line.setY( fath_gfx.y );
-
-
-			addFamMap.incrementEdges(
-				u_matesline, fath.id, moth.id, 0,
-				unique_graph_objs[this._family].edges,
-				new_line
-			)
-			new_line.setZIndex(1);
-
-			//reset
-			this.endLineDraw();
-			main_layer.draw();
 		}
+
+		var fam_group = unique_graph_objs[this._family].group,
+			group_pos = fam_group.getAbsolutePosition(),
+			new_line = this._tmpLine.clone();
+
+		fam_group.add(new_line);
+
+		var fath_gfx = unique_graph_objs[this._family].nodes[moth.id].graphics.getPosition();
+
+		new_line.setX( fath_gfx.x );
+		new_line.setY( fath_gfx.y );
+
+
+		addFamMap.incrementEdges(
+			u_matesline, fath.id, moth.id, 0,
+			unique_graph_objs[this._family].edges,
+			new_line
+		)
+		new_line.setZIndex(1);
+
+		//reset
+		this.endLineDraw();
+		redrawNodes(fath.id, this._family, true);
+		main_layer.draw();
 	}
+
+
+
 };
