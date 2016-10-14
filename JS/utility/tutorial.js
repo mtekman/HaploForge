fe12774f3972e88daff9d6731e78ac67749ff443
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-10-14 14:20:19 +0100

    tutorials in the making

---
+++ JS/utility/tutorial.js
@@ -0,0 +1,25 @@
+
+
+var Tutorial = {
+
+	// Frozen yellow hangar
+	yellowBox : function(header, small_text, abs_position,
+		onclick = null, onhover = null){
+
+		if (userOpts.showTooltips){
+
+		}
+
+	},
+
+
+	// Obnoxious pointy thing
+	pointy : function(direction, abs_position,
+		onclick = null, onhover = null){
+
+		if (userOpts.showTooltips){
+
+		}
+
+	}
+}
