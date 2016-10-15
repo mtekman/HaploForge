cd5c7e6be97ac6b11e42ab07e85b77a535775bee
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-10-15 03:22:02 +0100

    Tutorials on demand

--- JS/utility/tutorial.js
+++ JS/utility/tutorial.js
@@ -1,25 +1,76 @@
 
 
-var Tutorial = {
+class Tutorial {
 
-	// Frozen yellow hangar
-	yellowBox : function(header, small_text, abs_position,
-		onclick = null, onhover = null){
+	constructor(id, direct, head, text, pos){
 
-		if (userOpts.showTooltips){
+		this.box = Tutorial.__makePointy(id, direct, head, text);
 
+		this.box.style.position = "absolute";
+		this.box.style.top  = pos.top  + 'px';
+		this.box.style.left = pos.left + 'px';
+
+		this.__shake();
+	}
+
+
+
+	// Smacky
+	__shake(){
+
+		var that = this,
+			box = that.box;
+
+		var move = 2,
+			mult = 2,
+			ydiff= 10;
+
+		var box_bot = box.offsetTop,
+			box_top = box_bot - ydiff;
+
+
+		var tim = setInterval(function(){
+			var current = box.offsetTop;
+		  	
+		  	if (current < box_top){
+		    	move = mult;
+		  	}
+		  	else if (current > box_bot ){
+		    	move = -mult;
+		  	}
+		  	that.box.style.top = (current + move) + 'px';
+		}, 100);
+
+		box.onclick = function(){
+			clearInterval(tim);
+			this.parentNode.removeChild(this);
 		}
+	}
 
-	},
+
+	// new block
+	static __makePointy(id, direct, head, text){
+		var div1 = document.createElement('div');
+		div1.id = id;
+		div1.className = 'tutorial ' + direct;
+		document.getElementsByTagName('body')[0].appendChild(div1);
+
+		// Now create and append to iDiv
+		var divtext = document.createElement('div');
+		divtext.className = 'tutorial_text';
+
+		var h2er = document.createElement('h4');
+		h2er.innerText = head;
+
+		var per = document.createElement('p');
+		per.innerText = text;
 
 
-	// Obnoxious pointy thing
-	pointy : function(direction, abs_position,
-		onclick = null, onhover = null){
+		divtext.appendChild(h2er);
+		divtext.appendChild(per);
 
-		if (userOpts.showTooltips){
+		div1.appendChild(divtext)
 
-		}
-
+		return div1;
 	}
 }
