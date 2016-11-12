def69cdda78c3035ae914fa8c3fa68b5c44d25e6
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-11-12 23:58:21 +0000

    test tutorials

--- JS/test.js
+++ JS/test.js
@@ -1,3 +1,20 @@
+var pages = [
+	["Title1", "This is the text at the top that explains quickly", 
+	 "This is the bottom text that rambles on and on about nothing in particular and most people think is a bit much to be honest", null],
+
+ 	["Title2", "This is the text at the top that explains quickly", 
+	 "This is the bottom text that rambles on and on about nothing in particular and most people think is a bit much to be honest", null],
+
+ 	["Title3", "This is the text at the top that explains quickly", 
+	 "This is the bottom text that rambles on and on about nothing in particular and most people think is a bit much to be honest", null],
+
+
+];
+
+var pf = new Tutorial( pages );
+
+
+
 
 var Test = {
 
