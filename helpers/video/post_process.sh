cb3290b0897e544fce19ed0990226fab4db42266
Mehmet Tekman <m.tekman@ucl.ac.uk>
2016-11-23 22:30:22 +0000

    using VP8 encoding now

--- helpers/video/post_process.sh
+++ helpers/video/post_process.sh
@@ -4,4 +4,5 @@ inp=$1
 [ "$inp" = "" ] && echo "`basename $0` <input.mp4>" && exit -1
 
 #ffmpeg -i $inp -c:v libx265 -vf scale=iw/2:-1 `basename $inp .mp4`.post.mp4
-ffmpeg -i $inp -c:v libx265 -vf format=yuv420p -vf scale=iw/1.5:-1 post/`basename $inp .mp4`.post.mp4
+#ffmpeg -i $inp -c:v libx264 -vf format=yuv420p -vf scale=iw/1.5:-1 post/`basename $inp .mp4`.post.mp4
+ffmpeg -i $inp -c:v libvpx -vf format=yuv420p -vf scale=iw/1.5:-1 post/`basename $inp .mp4`.post.webm
