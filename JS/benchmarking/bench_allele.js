a6cc7c1db28e7c8c6fe232e474ff1e65331b8a4f
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-06-04 22:50:13 +0100

    num root individuals, allele sizes, number of generations

--- JS/benchmarking/bench_allele.js
+++ JS/benchmarking/bench_allele.js
@@ -1,47 +1,57 @@
 function randomIndex(len){
-	return Math.floor(Math.random() * len);
+    return Math.floor(Math.random() * len);
 }
 
 
 var BenchAllele  = {
 
-	disease_allele : null,
+    disease_allele : null,
+    allele_size : 16,
+    __alleles_in_use : {},
 
 
-
-	newNonDiseaseAllele: function()
+    newNonDiseaseAllele: function()
+    {
+	if (BenchAllele.disease_allele === null)
 	{
-		if(BenchAllele.disease_allele === null){ 
-			BenchAllele.disease_allele = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-			console.log("Defining new disease_allele", BenchAllele.disease_allele);
-		 };
+	    BenchAllele.disease_allele = [];
+	    for (let i=0; i < BenchAllele.allele_size; i++){ BenchAllele.disease_allele.push(1); }
+	    
+	    BenchAllele.__alleles_in_use[ BenchAllele.disease_allele.join("") ] = true;
 
-		let new_all = [for (i of BenchAllele.disease_allele) [1,2][randomIndex(2)]]
+	    console.log("Defining new disease_allele", BenchAllele.disease_allele);
+	};
 
-		if (BenchAllele.disease_allele.join("") === new_all.join("")){
-			return BenchAllele.newNonDiseaseAllele();
-		}
+	let new_all = [for (i of BenchAllele.disease_allele) [1,2][randomIndex(2)]]
+	let key = new_all.join("")
 
-		return new_all;
-	},
-
-	performMeiosis(all1,all2){
-		if ( all1.length !== all2.length){
-			console.error("Allele lengths do not match");
-			return 0;
-		}
-
-		let buff = Math.floor(all1.length / 2);
-		let allele_len = buff + all1.length + buff; // buffer on each side
-		// if number falls in 0:5 or -5:1, then no recombination occurs
-
-		let index_split = randomIndex(allele_len);
-
-		if (index_split < buff)             { return all1;}
-		if (index_split > allele_len - buff){ return all2;}
-
-		// Otherwise recombination
-		index_split -= buff;
-		return all1.slice(0,index_split).concat(all2.slice(index_split,));
+	// Each FA must be unique
+	if (key in BenchAllele.__alleles_in_use){
+	    return BenchAllele.newNonDiseaseAllele();
 	}
+
+	BenchAllele.__alleles_in_use[key] = true
+	return new_all;
+    },
+
+    performMeiosis: function(all1,all2)
+    {
+	if ( all1.length !== all2.length){
+	    console.error("Allele lengths do not match");
+	    return 0;
+	}
+
+	let buff = Math.floor(all1.length / 2);
+	let allele_len = buff + all1.length + buff; // buffer on each side
+	// if number falls in 0:5 or -5:1, then no recombination occurs
+
+	let index_split = randomIndex(allele_len);
+
+	if (index_split < buff)             { return all1;}
+	if (index_split > allele_len - buff){ return all2;}
+
+	// Otherwise recombination
+	index_split -= buff;
+	return all1.slice(0,index_split).concat(all2.slice(index_split,));
+    }
 }
--- JS/benchmarking/pedigree_generator.js
+++ JS/benchmarking/pedigree_generator.js
@@ -1,158 +1,187 @@
 
 class TreeGenerator {
 
-	constructor(){
-		this.max_generations = 8;
-		this.chance_of_inbreeding = 0.1;
-
-		// To be filled
-		this.num_people = 0;
-
-		let root_indiv = new BenchPerson(0); // generation 0
-
-		this.germinate(root_indiv);
+    constructor(num_roots, num_gens, allele_size = 16, inbreed_chance = 0.1)
+    {
+	if (num_gens === undefined){
+	    console.log("give root and generations");
+	    return 0;
 	}
 
-	germinate(perc){
-		console.log("Germinate", perc)
-		// Perc is a pre-existing individual of unknown gender
+	BenchAllele.allele_size = allele_size;
+	
+	this.max_generations = num_gens;
+	this.chance_of_inbreeding = inbreed_chance;
 
-		// first generations have higher chance of mating and producing more kids
-		let generation = perc.generation
-		let gen_modif = this.max_generations - generation;	
+	// To be filled
+	this.num_people = 0;
+	this.inbred_couples = {};
 
-		if (gen_modif === 0){
-			// reached max generation
-			return 0
-		}
-
-		let hasMate = (gen_modif * Math.random()) > 0.5;
-		if (hasMate){
-			let perc_gender = perc.gender,
-				mate_gender = perc_gender===2?1:2;
-
-			let mate = null;			
-			if (this.chance_of_inbreeding < Math.random()){
-				// Mate is a cousin
-				mate = perc.findAvailableCousin()
-				console.log("cousin mate", mate);
-			}
-
-			if (mate === null){ // No cousin match, or new founder
-				// Mate is a new founder
-				mate = new BenchPerson(generation, mate_gender, 0, 0)
-				console.log("random mate", mate);
-			}
-
-			perc.addMate(mate);
-			mate.addMate(perc);
-
-			let father = perc_gender===1?perc:mate;
-			let mother = perc_gender===2?perc:mate;
-
-			// All mates must produce at least 1 offspring 
-			let numOff = 1 + randomIndex(gen_modif)
-			for (let n=0; n < numOff; n++)
-			{
-				let child = new BenchPerson(generation+1, 0, mother.id, father.id)
-				perc.addChild(child);
-				mate.addChild(child);
-
-				// and recurse
-				this.germinate(child)
-			}
-		}
-		return 0;
+	for (let i=0; i < num_roots; i++)
+	{
+	    let root_indiv = new BenchPerson(1); // generation 0
+	    this.germinate(root_indiv);    
 	}
+
+	this.printmetrics()
+    }
+
+    printmetrics(){
+	console.log("Num people:", this.num_people)
+
+	let inco = [ for (i of Object.keys(this.inbred_couples)) i];
+	
+	console.log("Inbred couples", inco.length, inco)
+	console.log("Generations",
+		    [for (i of Object.keys(BenchPerson.generation_map))
+		     i+": "+Object.keys(BenchPerson.generation_map[i]).length ]
+		   )
+    }
+    
+
+    germinate(perc){
+	// Perc is a pre-existing individual of unknown gender
+
+	// first generations have higher chance of mating and producing more kids
+	// lower generations have higher chance of inbreeding
+	let generation = perc.generation
+	let gen_modif = this.max_generations - generation;
+
+	this.num_people += 1
+	//console.log("Germinate", perc.id, generation, gen_modif, this.num_people)
+
+	if (gen_modif === 0){
+	    // reached max generation
+	    return 0
+	}
+
+	let hasMate = (gen_modif * Math.random()) > 0.5;
+	if (hasMate){
+	    let perc_gender = perc.gender,
+		mate_gender = perc_gender===2?1:2;
+
+	    let mate = null;			
+	    if (this.chance_of_inbreeding * (generation / this.max_generations ) > Math.random()){
+		// Mate is a cousin
+		mate = perc.findAvailableCousin()
+	    }
+
+	    if (mate !== null){
+		this.inbred_couples[perc.id + "__" + mate.id] = true
+	    }
+	    else {
+		// Mate is a new founder
+		mate = new BenchPerson(generation, mate_gender, null, null)
+		//console.log("random mate", mate);
+	    }
+
+	    perc.addMate(mate);
+	    mate.addMate(perc);
+
+	    let father = perc_gender===1?perc:mate;
+	    let mother = perc_gender===2?perc:mate;
+
+	    // All mates must produce at least 1 offspring 
+	    let numOff = 1 + randomIndex(gen_modif)
+	    //console.log("   kids:", numOff)
+	    for (let n=0; n < numOff; n++)
+	    {
+		let child = new BenchPerson(generation + 1, 0, mother, father)
+		perc.addChild(child);
+		mate.addChild(child);
+
+		// and recurse
+		this.germinate(child)
+	    }
+	}
+	return 0;
+    }
 }
 
 
 class BenchPerson  extends Person {
 
-	init(){
-		if (BenchPerson.generation_map === undefined){
-			BenchPerson.generation_map = {}
-		}
+    init(){
+	if (BenchPerson.generation_map === undefined){
+	    BenchPerson.generation_map = {}
+	}
+    }
+
+    constructor(generation, gender = 0, mother = null, father = null) {
+	super();
+
+	this.init();
+
+	this.generation = generation;
+	this.gender = ((gender===0)?this.randomGender():gender);
+	
+	this.id = this.randomID()
+
+	this.mother = mother
+	this.father = father
+	this.affected = 1;  // innocent until proven guilty
+
+
+	if (this.mother !== null){         // Non-Founder
+	    this.simulateMeiosis()
+	}
+	else {
+	    this.generateFounderAlleles(); // Founder
+	}
+    }
+
+    findAvailableCousin(){
+	let cousins = BenchPerson.generation_map[this.generation]; // Same generation
+
+	for (let c_id in cousins)
+	{
+	    let cousin = cousins[c_id];
+
+	    if (cousin.mother === null){ continue; }    
+	    if (cousin.mother.id === this.mother.id || cousin.father.id === this.father.id){ continue; } // Not Sibs
+	    if (cousin.gender === this.gender){ continue; } // Straight
+	    if (cousin.mates.length > 0){ continue; } // Available
+	    //  console.log("inbreeding:", this.id, cousin.id);	    
+	    return cousin;
+	}
+	return null; // no matches found
+    }
+
+
+    simulateMeiosis(){
+	this.haplo_data.push( BenchAllele.performMeiosis(
+	    this.mother.haplo_data[0], this.mother.haplo_data[1]) )
+	this.haplo_data.push( BenchAllele.performMeiosis(
+	    this.father.haplo_data[0], this.father.haplo_data[1]) )
+    }
+
+    generateFounderAlleles(){
+	this.haplo_data.push( BenchAllele.newNonDiseaseAllele() )
+	this.haplo_data.push( BenchAllele.newNonDiseaseAllele() )
+    }
+
+    randomGender(){
+	return 1 + randomIndex(2);
+    }
+
+    randomID(modif = 1){
+	
+	let ext = this.gender * modif, // males odd, females even,
+	    num = (this.generation * 1000) + ext;
+
+	//console.log("generation", this.generation, num, modif, ext)
+	if (!(this.generation in BenchPerson.generation_map)){
+	    BenchPerson.generation_map[this.generation] = {}
 	}
 
-	constructor(generation, gender = 0, mother = null, father = null) {
-		super();
-
-		this.init();
-
-		this.generation = generation;
-		this.gender = gender === 0?this.randomGender():gender;
-		
-		this.id = this.randomID(generation, gender)
-
-		this.mother = mother
-		this.father = father
-		this.affected = 1;  // innocent until proven guilty
-
-
-		if (this.mother !== null){         // Non-Founder
-			this.haplo_data = this.simulateMeiosis()
-		}
-		else {
-			this.haplo_data = this.generateFounderAlleles(); // Founder
-		}
+	if (num in BenchPerson.generation_map[this.generation]){
+	    return this.randomID(modif + 1);
 	}
 
-	findAvailableCousin(){
-		let cousins = BenchPerson.generation_map[this.generation]; // Same generation
-
-		for (let c_id in cousins){
-			let cousin = cousins[c_id];
-
-			if (cousin.mother === null){ 
-				continue;
-			}
-
-			if (cousin.mother.id !== this.mother.id || cousin.father.id !== this.father.id){
-				// Not sibs or half-sibs
-				if (cousin.gender !== this.gender){   // straight
-					if (!cousin.hasChild){             // single
-						console.log("inbreeding:", this.id, cousin.id);
-						return cousin;
-					}
-				}
-			}
-		}
-		// no matches found
-		return null;
-	}
-
-
-	simulateMeiosis(){
-		this.haplo_data.push( BenchAllele.performMeiosis(this.mother.haplo_data[0], this.mother.haplo_data[1]) )
-		this.haplo_data.push( BenchAllele.performMeiosis(this.father.haplo_data[0], this.father.haplo_data[1]) )
-	}
-
-	generateFounderAlleles(){
-		this.haplo_data.push( BenchAllele.newNonDiseaseAllele() )
-		this.haplo_data.push( BenchAllele.newNonDiseaseAllele() )
-	}
-
-	randomGender(){
-		return 1 + randomIndex(2);
-	}
-
-	randomID(generation, gender, modif = 1){
-		let ext = gender * modif, // males odd, females even,
-			num = (generation * 1000) + ext;
-
-		if (!(generation in BenchPerson.generation_map)){
-			BenchPerson.generation_map[generation] = {}
-		}
-		
-		if (!(num in BenchPerson.generation_map[generation])){
-			BenchPerson.generation_map[generation][num] = this;
-			return num;
-		} else {
-			// recurse
-			return this.randomID(generation, gender, modif + 1);
-		}
-	}
+	// Otherwise result
+	BenchPerson.generation_map[this.generation][num] = this;
+	return num
+    }
 }
 
 
