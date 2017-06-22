a6b7b4894dfb6d33ad4d4eaa96e3a42e200f7819
Mehmet Tekman <m.tekman@ucl.ac.uk>
2017-06-22 18:48:05 +0100

    added mousewheel listener

---
+++ JS/iofuncs/mouseresize.js
@@ -0,0 +1,51 @@
+
+var MouseResize = {
+
+	resize_modes : {
+		pedcreate: ModeTracker.modes.pedcreate,
+		haploview: ModeTracker.modes.haploview,
+	},
+
+	_prevwheelstate : 0,
+	_scale : 1,
+
+	_wheelHandler: function(event){
+
+		var delta = event.detail;
+		if (delta === 0){
+			delta = (event.deltaY > 0)?-0.1:0.1;
+		}
+
+		var wheelstatechanged = false;
+
+		if ((delta > 0 && MouseResize._prevwheelstate < 0)
+		  ||(delta < 0 && MouseResize._prevwheelstate > 0))
+		{
+			wheelstatechanged = true
+		}
+
+		if (!wheelstatechanged){
+			let new_scale = main_layer.getScale().x + delta;
+			
+			main_layer.setScale({
+				x:new_scale,y:new_scale
+			});
+			utility.notify("Scale", new_scale.toFixed(1))
+			main_layer.draw();
+		}
+		MouseResize._prevwheelstate = delta;
+	},
+
+	on : function(handler)
+	{
+		MouseWheel.on(  MouseResize._wheelHandler );
+		stage.setDraggable(true)
+	},
+
+	off: function(handler)
+	{
+		MouseWheel.off( MouseResize._wheelHandler );
+		stage.setDraggable(false)
+	}
+}
+
---
+++ JS/iofuncs/mousewheel.js
@@ -0,0 +1,46 @@
+
+var MouseWheel = {
+	
+	__mousewheellistener_on : false,
+	__lasthandler : null,
+
+	_addMouseWheel: function(handler)
+	{
+		if (document.addEventListener) {
+        	document.addEventListener("mousewheel", handler, false); //IE9, Chrome, Safari, Oper
+        	document.addEventListener("wheel", handler, false); //Firefox
+    	} else {
+        	document.attachEvent("onmousewheel", handler); //IE 6/7/8
+    	}
+		MouseWheel.__mousewheellistener_on = true;
+		MouseWheel.__lasthandler = handler;
+	},
+
+	_removeMouseWheel: function(handler)
+	{
+		if (document.addEventListener) {
+        	document.removeEventListener("mousewheel", handler, false); //IE9, Chrome, Safari, Oper
+        	document.removeEventListener("wheel", handler, false); //Firefox
+    	} else {
+        	document.detachEvent("onmousewheel", handler); //IE 6/7/8
+    	}
+		MouseWheel.__mousewheellistener_on = false;
+	},
+
+	on : function(handler){
+		if (MouseWheel.__mousewheellistener_on){
+			console.log("Listener already active, detaching last handler and replacing with new");
+			MouseWheel._removeMouseWheel( MouseWheel.__lasthandler );
+		}
+		MouseWheel._addMouseWheel( handler );
+	},
+
+	off : function(handler){
+		if (MouseWheel.__mousewheellistener_on){
+			MouseWheel._removeMouseWheel( handler );
+			console.log("turning off mouse");
+			return 0;
+		}
+		console.log("already off");
+	}
+}
